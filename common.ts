d1af0ee55d5dc48fb4b314fa1908790d2ba65788
BytedanceQA <yanweicheng@bytedance.com>
2024-08-30 07:44:29 +0000

    增加测试文件，多函数生成单测

--- common.ts
+++ common.ts
@@ -21,7 +21,19 @@ function quickSort(arr: number[]): number[] {
     return [...quickSort(less), ...equal, ...quickSort(greater)];
   }
 
-
+  function bubbleSort(arr: number[]): number[] {
+    const n = arr.length;
+    for (let i = 0; i < n - 1; i++) {
+      for (let j = 0; j < n - i - 1; j++) {
+        if (arr[j] > arr[j + 1]) {
+          const temp = arr[j];
+          arr[j] = arr[j + 1];
+          arr[j + 1] = temp;
+        }
+      }
+    }
+    return arr;
+  }
 
 function convertNumbersToInt(str: string): string {
    str.replace(/\d+/g, (match) => parseInt(match, 10).toString());
